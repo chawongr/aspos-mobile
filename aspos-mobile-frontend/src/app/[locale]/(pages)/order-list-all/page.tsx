8d3ff1878a7510bdfc8128abe1f06313bd1f3cc2
bbbaipooo <baipoo_B@hotmail.com>
2025-01-05 03:22:12 +0700

    fix(order-list):finish

--- aspos-mobile-frontend/src/app/[locale]/(pages)/order-list-all/page.tsx
+++ aspos-mobile-frontend/src/app/[locale]/(pages)/order-list-all/page.tsx
@@ -1,44 +1,81 @@
+'use client'
+
 import React from 'react';
-import OrderCard from '../../components/order-card';
-import MenuCard from '../../components/menu-card';
-import { food2 } from '@/app/[locale]/components/all-image'
+import OrderCard from '../../components/order-all-card';
+import { Food2 } from '@/app/[locale]/components/all-image'
+import { IoClose } from "react-icons/io5";
+import OrderData from '../../datas/order.json';
+import { CiViewList } from "react-icons/ci";
+import Link from 'next/link';
+import { usePathname, useRouter } from "next/navigation";
+
+interface Order{
+  id: number;
+  imageUrl: string;
+  title: string;
+  description: string;
+  price: number;
+  quantity: number;
+  status: string;
+}
 
 export default function OrderList() {
-  const orders = [
-    {
-      imageUrl: food2,
-      title: 'ข้าวผัดกะเพราทะเลไข่ข้น',
-      description:
-        'ข้าวกะเพราหมูหั่นชิ้น(คลุก)ใช้พริกแห้งและซอกะเพราสูตรพันปีลับเฉพาะร้าน',
-      price: 129,
-      quantity: 1,
-      status:"กำลังทำ"
-    },
-  ];
+  const path = usePathname().substring(1);
+  const lang = path.split('/')[0];
+
+  const orders: Order[] = OrderData;
+
+  const totalAmount={
+    amount: 15,
+    price: 1352,
+    time: "19:01:00"
+  }
+
+  // Format time function
+  const formatTime = (time) => {
+    const [hours, minutes, seconds] = time.split(":");
+    return `${hours}:${minutes}`; // Remove seconds
+  };
 
   return (
-    <div >
-      {orders.map((order, index) => (
-        <OrderCard
-          key={index}
-          imageUrl={order.imageUrl}
-          title={order.title}
-          description={order.description}
-          price={order.price}
-          quantity={order.quantity}
-          status={order.status}
-        />
-      ))}
-      {orders.map((order, index) => (
-        <MenuCard
-          key={index}
-          imageUrl={order.imageUrl}
-          title={order.title}
-          description={order.description}
-          price={order.price}
-          quantity={order.quantity}
-        />
-      ))}
+    <div className="flex flex-col relative">
+      <header className='sticky top-0 w-full-p-5 md:w-full-p-10 py-3 md:py-7 flex justify-between items-center gap-2 md:gap-4 bg-white' >
+        <Link href={`/${lang}`}>
+          <IoClose className="w-7 h-7 md:w-9 md:h-9" />
+        </Link>
+        <div className='flex flex-col items-center'>
+          <h1 className="text-xl md:text-3xl font-semibold">รายการทั้งหมด</h1>
+          <p className="text-sm md:text-lg text-gray-500">ข้อมูล ณ เวลา {formatTime(totalAmount.time)}</p>
+        </div>
+        <div className="w-7 h-7 md:w-9 md:h-9"/>
+      </header>
+
+      <div className="flex-1 overflow-y-auto">
+        {orders.map((order, index) => (
+          <div key={order.id} className={`${index !== orders.length - 1 ? 'border-b-[0.5px] border-borderGray ' : ''}`} >
+          <OrderCard
+            key={order.id}
+            imageUrl={Food2}
+            title={order.title}
+            description={order.description}
+            price={order.price}
+            quantity={order.quantity}
+            status={order.status}
+          />
+          </div>
+        ))}
+      </div>
+
+      <footer className='sticky bottom-0 w-full-p-5 md:w-full-p-10 py-6 md:py-10 flex flex-col gap-2 md:gap-4 bg-white' >
+        <div className='flex justify-between text-lg md:text-2xl font-medium'>
+          <div>{totalAmount.amount} รายการ</div>
+          <div>฿{totalAmount.price}</div>
+        </div>
+        <button className="py-4 md:py-5 text-base md:text-xl font-medium text-white bg-green rounded-full w-full flex items-center justify-center">
+          <CiViewList className="w-5 h-5 md:w-6 md:h-6 mr-2 md:mr-3"/> 
+          บิลค่าอาหาร
+        </button>
+      </footer>
     </div>
   );
 }
--- aspos-mobile-frontend/src/app/[locale]/components/all-image.tsx
+++ aspos-mobile-frontend/src/app/[locale]/components/all-image.tsx
@@ -1,9 +1,9 @@
 import ThFlag from '../../../../public/th-flag.jpg'
 import EnFlag from '../../../../public/en-flag.jpg'
 import Food1 from '../../../../public/food1.jpg'
-import food2 from '../../../../public/food2.jpg'
-import food3 from '../../../../public/food3.jpg'
-import food4 from '../../../../public/food4.jpg'
+import Food2 from '../../../../public/food2.jpg'
+import Food3 from '../../../../public/food3.jpg'
+import Food4 from '../../../../public/food4.jpg'
 import Bell from '../../../../public/bell.svg'
 import Bill from '../../../../public/bill.svg'
 
@@ -11,9 +11,9 @@ export {
     ThFlag,
     EnFlag,
     Food1,
-    food2,
-    food3,
-    food4,
+    Food2,
+    Food3,
+    Food4,
     Bell,
     Bill
 }
